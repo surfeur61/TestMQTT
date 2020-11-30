cb4ea80716940fb9e333382b0a27e0bf86f95205
surfeur61 <68653617+surfeur61@users.noreply.github.com>
2020-11-30 15:46:48 +0100

    Update CLIENTMQTTsubscriber.py

--- CLIENTMQTTsubscriber.py
+++ CLIENTMQTTsubscriber.py
@@ -2,17 +2,12 @@ import paho.mqtt.client as mqtt
 import os
 import time
 
-
-#broker=os.environ.get('K3S_SERVICE_BROKER','10.0.1.8')
-broker=["10.0.1.8","10.0.1.9","10.0.1.11","10.0.1.12"]
+broker=os.environ.get('K3S_SERVICE_BROKER','10.0.1.8')
 
 # The callback for when the client receives a CONNACK response from the server.
 def on_connect(client, userdata, flags, rc):
-    if rc==0:
-                client.connected_flag=True #set flag
-                print("connected OK")
-        else:
-                print("Bad connection Returned code=",rc)
+        if rc==0:
+            client.connected_flag=True #set flag
 
     # Subscribing in on_connect() means that if we lose the connection and
     # reconnect then subscriptions will be renewed.
@@ -23,20 +18,8 @@ def on_message(client, userdata, msg):
     print(msg.topic+" "+str(msg.payload))
     
 def connection():
-        c=0
-        while not client.connected_flag:
-                try :
-                        print 'try'
-                        client.connect(broker[c],1883,60)
-                        client.loop_start()
-                        while client.connected_flag==False:
-                                time.sleep(3)
-                except:
-                        c+=1
-                        pass
-
-                if c==len(broker):
-                        c=0
+        client.connect(broker,1883,60)
+        client.loop_start()
 
 client = mqtt.Client()
 client.on_connect = on_connect
